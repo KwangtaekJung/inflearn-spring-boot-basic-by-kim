5388be1565f1e714b3a0232814b1d5ccef1cc993
kt79.jung <kt79.jung@gmail.com>
2021-07-03 13:53:55 +0900

    MemberService and Test with MemoryMemberRepository

--- src/main/java/com/example/service/MemberService.java
+++ src/main/java/com/example/service/MemberService.java
@@ -4,26 +4,52 @@ import com.example.domain.Member;
 import com.example.repository.MemberRepository;
 import com.example.repository.MemoryMemberRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
     public Long join(Member member) {
 
-        validatDuplicateMember(member);  // 중복 회원 검증
+        validateDuplicateMember(member);  // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
 
-    private void validatDuplicateMember(Member member) {
+    private void validateDuplicateMember(Member member) {
         // 같은 이름의 회원 가입 불가
         memberRepository.findByName(member.getName())
             .ifPresent(m -> {
-                new IllegalArgumentException("이미 존재하는 회원입니다.");
+                throw new IllegalArgumentException("이미 존재하는 회원입니다.");
                 });
     }
 
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
 
+    /**
+     * Id로 회원 조회
+     * @param memberId
+     * @return
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
