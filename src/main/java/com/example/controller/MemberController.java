1352bcc2aad0245b538e2a4a18d6c6136d0d94af
KwangtaekJung <kt79.jung@gmail.com>
2021-07-04 15:29:35 +0900

    회원 웹 기능 - 조회

--- src/main/java/com/example/controller/MemberController.java
+++ src/main/java/com/example/controller/MemberController.java
@@ -4,8 +4,12 @@ import com.example.domain.Member;
 import com.example.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,10 +27,17 @@ public class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String create(MemberForm form) {
+    public String create(@ModelAttribute MemberForm form) {  // @ModelAttribute는 생략 가능
         Member member = new Member();
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
