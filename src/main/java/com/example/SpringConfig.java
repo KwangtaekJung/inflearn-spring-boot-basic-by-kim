86d6c6fe4101e45ad7bed52fcafb2b64227e8151
KwangtaekJung <kt79.jung@gmail.com>
2021-07-04 16:34:17 +0900

    순수 JDBC

--- src/main/java/com/example/SpringConfig.java
+++ src/main/java/com/example/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import com.example.repository.JdbcMemberRepository;
 import com.example.repository.MemberRepository;
 import com.example.repository.MemoryMemberRepository;
 import com.example.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
