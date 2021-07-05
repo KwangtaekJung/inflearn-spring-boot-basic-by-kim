88bbc7919ea9da8508bc9d43c7cd2fa69548a169
KwangtaekJung <kt79.jung@gmail.com>
2021-07-05 10:07:58 +0900

    JPA

--- src/main/java/com/example/service/MemberService.java
+++ src/main/java/com/example/service/MemberService.java
@@ -5,12 +5,14 @@ import com.example.repository.MemberRepository;
 import com.example.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 //@Service
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
