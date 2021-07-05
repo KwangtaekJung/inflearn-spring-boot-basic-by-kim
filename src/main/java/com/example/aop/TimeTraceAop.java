733b4a43fcbdf10e8d59c3961fc4e07da9d5ff39
KwangtaekJung <kt79.jung@gmail.com>
2021-07-05 13:33:05 +0900

    AOP

---
+++ src/main/java/com/example/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package com.example.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component  // SpringConfig에서 @Bean으로 등록하면 안되네. 왜?
+public class TimeTraceAop {
+
+    @Around("execution(* com.example..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
